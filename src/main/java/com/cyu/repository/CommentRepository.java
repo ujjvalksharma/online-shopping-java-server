475787dc006cfeec5a75ccd6be56604cb196ca0e
Ujjval Sharma <ujjvalksharma@gmail.com>
2022-04-06 10:29:21 -0400

    Update CommentRepository.java

--- src/main/java/com/cyu/repository/CommentRepository.java
+++ src/main/java/com/cyu/repository/CommentRepository.java
@@ -8,14 +8,23 @@ import org.springframework.data.jpa.repository.Query;
 import com.cyu.model.Comment;
 import com.cyu.model.Person;
 
+@Repository : this means that CommentRepository is A repository is a mechanism for encapsulating storage, 
+                   retrieval, and search behavior which emulates a collection of objects.
+	      JpaRepository is a JPA (Java Persistence API) specific extension of Repository. 
+	     It contains the full API of CrudRepository and PagingAndSortingRepository.
+			   
+			                                 <database_object_class,primary_key>
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
 	  @Query(  value = "select * from Comment where prod_id=?1",
-			  nativeQuery = true)
+			  nativeQuery = true) // to write query
 	  public List<Comment> getCommentsByProductId(int productId); 
 	  
 	  @Query(  value = "select * from Comment where user_id=?1",
 			  nativeQuery = true)
 	  public List<Comment> getCommentsByUserId(int userId);
+	  
+	  
+	  public Comment findByValue(String value); //immbeded method query
 	  
 }
