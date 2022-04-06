a31d444746872762b0aaf07c0e21f23d8a40a125
Ujjval Sharma <ujjvalksharma@gmail.com>
2022-04-06 10:37:54 -0400

    Update Application.java

--- src/main/java/com/cyu/Application.java
+++ src/main/java/com/cyu/Application.java
@@ -3,7 +3,17 @@ package com.cyu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication : @Configuration, @EnableAutoConfiguration and @ComponentScan
+	
+Component: @Resposity,@Service, @ResController, @Controller,@Compoenent : This is used to register the class object as a bean 
+	                                                                  which can be autoriwed to get singleton object
+	
+	@ComponentScan: is used by spring to scan all the componenets in the class path
+		
+		@EnableAutoConfiguration: it automatically creates and registers beans based on both the 
+		included jar files in the classpath and the beans defined by us.
+			
+			@Configration: is used defined some config for the project like spring security.
 public class Application {
 
 	public static void main(String[] args) {
